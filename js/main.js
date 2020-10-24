df9ebb90003767424d5f286bfeaa2e9254b0700a
AlexSavchuk95 <savchukalex57@gmail.com>
2020-10-24 14:11:55 +0300

    safari scroll

--- js/main.js
+++ js/main.js
@@ -9,22 +9,6 @@ menuToggle.onclick = function () {
 }
 
 
-const anchors = document.querySelectorAll('a[href*="#"]')
-
-for (let anchor of anchors) {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    
-    const blockID = anchor.getAttribute('href').substr(1)
-    
-    document.getElementById(blockID).scrollIntoView({
-      behavior: 'smooth',
-      block: 'start'
-    })
-  })
-}
-
-
 var slideShow = (function () {
     return function (selector, config) {
       var
